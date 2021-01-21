b60071c62da5c6ce921d0e41e01fab8f4d635ad3
vishal-1809 <77793956+vishal-1809@users.noreply.github.com>
2021-01-21 21:43:44 +0530

    Update notes.js

--- notes.js
+++ notes.js
@@ -66,11 +66,15 @@ let search = document.getElementById('searchTxt');
 search.addEventListener("input", function(){
 
     let inputVal = search.value.toLowerCase();
+    let inputValu = search.value.toUpperCase();
     // console.log('Input event fired!', inputVal);
     let noteCards = document.getElementsByClassName('noteCard');
     Array.from(noteCards).forEach(function(element){
         let cardTxt = element.getElementsByTagName("p")[0].innerText;
         if(cardTxt.includes(inputVal)){
+            element.style.display = "block";
+        }
+        else if(cardTxt.includes(inputValu)){
             element.style.display = "block";
         }
         else{
